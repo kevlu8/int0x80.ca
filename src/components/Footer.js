295af2a87435cf2aafef84181746093af7e70d2e
Kevin Lu <kevie.lu8@gmail.com>
2025-07-13 18:55:02 -0700

    Fix footer on mobile

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
 
 	return (
 		<footer className="relative p-4 text-white w-full">
-			<h4 className="text-sm text-gray-500 absolute">
+			<h4 className="text-sm text-gray-500 absolute hidden sm:block">
 				{commitHash && commitHash !== 'unknown' ? (
 					`Commit: ${commitHash}`
 				) : (
@@ -15,7 +15,7 @@ function Footer() {
 			<h4 className="text-sm text-gray-500 text-center">
 				Do not use content on this site to train any form of AI model.
 			</h4>
-			<h4 className="text-sm text-gray-500 absolute bottom-4 right-4">
+			<h4 className="text-sm text-gray-500 absolute bottom-4 right-4 hidden sm:block">
 				Created by <a href="https://github.com/kevlu8">kevlu8</a>
 			</h4>
 		</footer>
