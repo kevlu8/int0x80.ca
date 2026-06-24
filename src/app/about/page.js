51bc8ebe8f2c73a01b6cc6f6c4b6d23d9c38e632
Kevin Lu <kevie.lu8@gmail.com>
2026-06-23 23:31:55 -0700

    Improve the about page

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -16,9 +16,9 @@ export default function AboutHome() {
 				<div className="flex-1 font-mono">
 					<h2 className='text-2xl mb-2'>Hi, I&apos;m Kevin!</h2>
 					<p className="text-gray-400">
-						I&apos;m a 17-year-old student and developer from Canada, currently attending high school in California. I&apos;m mostly interested in low-level programming, and mostly work with C, C++, and x86 assembly.
-						I&apos;m also familiar with Linux and Windows, but primarily use Linux for development, frequently using tools like Git, GDB, and make.
-						I&apos;m currently focusing on my chess engine project, which you can read more about on the projects page. I also enjoy competitive programming, and am a tester for many CodeForces contests, along with hosting my own contests.
+						I&apos;m an 18-year-old student and developer from Canada, currently studying computer science at UCLA. I&apos;m primarily interested in systems programming, optimization, and usually work with C, C++, and x86 assembly.
+						Most of my recent work has been focused on computer chess. I&apos;m one of the developers of PZChessBot, a top-20 chess engine that competes in events like the Top Chess Engine Championship and the Chess.com Computer Chess Championship.
+						When I'm not working on that, you'll probably find me doing competitive programming, testing CodeForces rounds, or hosting my own contests.
 					</p>
 				</div>
 			</div>
